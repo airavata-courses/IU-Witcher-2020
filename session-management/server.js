a40f45add6850022cfa953cf76d021548971d7cc
akshaygpt <iitb.akshay@gmail.com>
2020-02-12 13:21:51 -0500

    Deleting redundant code

--- session-management/server.js
+++ session-management/server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser') //middleware for reading html from nod
 const app = express()
 const mongoose = require('mongoose')
 const usersRouter = require('./routes/users')
+const historiesRouter = require('./routes/histories')
 
 //db connection
 mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true})
@@ -20,32 +21,9 @@ express.json()
 app.use(express.json())
 
 app.use('/users', usersRouter) //anything with the route 'root/users/anything/here' will go to usersRouter
+app.use('/histories', historiesRouter)
 
 //handlers
 app.listen(process.env.PORT || 4321, function() {
   console.log('Server started on port 4321')
 })
-
-// const users = [
-//     {id: 1, name: 'user 1'},
-//     {id: 2, name: 'nana 2'},
-//     {id: 3, name: 'saa 3'}
-// ]
-//
-// app.get('/home', (req, res) => {
-//     res.send('homepage')
-// })
-//
-// app.get('/', (req, res) => {
-//     res.sendFile(__dirname + '/index.html')
-// })
-//
-// app.get('/user/:id', (req, res) => {
-//     let user = users.find(u => u.id === parseInt(req.params.id))
-//     if(!user) res.status(404).send('No user with this id found') //404 error
-//     res.send(user)
-// })
-//
-// app.post('/addUser', (req, res) => {
-//     res.send('User added')
-// })
