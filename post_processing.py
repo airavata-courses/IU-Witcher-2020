5d3293530690a3f43c2ee6600015a896b13ca436
srajpal <srajpal@iu.edu>
2020-04-15 20:26:08 -0400

    Reaction time < 1s

--- post_processing.py
+++ post_processing.py
@@ -21,17 +21,11 @@ import pyimgur
 CLIENT_ID = "34e5e22dcc85836"
 image_link = pyimgur.Imgur(CLIENT_ID)
 
-
 # establishing connection to RabbitMQ server
 credentials = pika.PlainCredentials( username = 'guest' , password = 'guest' )
 connection = pika.BlockingConnection( pika.ConnectionParameters(
             host = 'message-broker' , port = 5672 , credentials = credentials ) )
 channel = connection.channel()
-
-# declaring receiving queue
-channel.queue_declare( queue = 'model_execution_2_post_processing' )
-# declaring sending queue
-channel.queue_declare( queue = 'post_processing_2_gateway' )
 
 # plotting Reflectivity range
 def plotting( plot_data ) :
@@ -65,18 +59,18 @@ def plotting( plot_data ) :
     uploaded_image = image_link.upload_image( 'Reflectivity_Correlation.png' , title = "Image Hosted" )
     return str( uploaded_image.link )
 
-def callback( ch , method , properties , body ) :
+def callback(ch, method, properties, body):
     user_data = json.loads( body )
-    image_url = plotting( user_data[ "Processing" ] )
+    image_url = ""#plotting( user_data[ "Processing" ] )
     processed_data = { "Forecast" : user_data[ "Forecast" ] , "url" : image_url }
     channel.basic_publish( exchange = '' , routing_key = 'post_processing_2_gateway' , body = json.dumps( processed_data ) )
-    connection.close( )
+    connection.close()
 
 while True :
     channel.basic_consume(
-        queue = 'model_execution_2_post_processing' , on_message_callback = callback , auto_ack = True )
-    channel.start_consuming( )
+        queue='model_execution_2_post_processing', on_message_callback=callback, auto_ack=True)
+    channel.start_consuming()
     print( "Post processed" )
-    connection = pika.BlockingConnection( pika.ConnectionParameters(
-                host = 'message-broker' , port = 5672 , credentials = credentials ) )
-    channel = connection.channel( )
+    connection = pika.BlockingConnection(pika.ConnectionParameters(
+                host = 'message-broker' , port=5672, credentials=credentials))
+    channel = connection.channel()
