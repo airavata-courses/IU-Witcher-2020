a4f49259b6632aef2ff1524c32b0b57c5fa0c53d
srajpal <srajpal@iu.edu>
2020-04-23 19:57:32 -0400

    Smaller code

--- post_processing.py
+++ post_processing.py
@@ -4,60 +4,11 @@ import sys
 import pika
 import json
 
-import numpy as np
-import matplotlib.pyplot as plt
-from numpy import ma
-
-from metpy.plots import ctables , add_timestamp
-from datetime import datetime
-
-import boto
-import boto3
-import boto.s3
-from boto.s3.key import Key
-from botocore.client import Config
-
-import pyimgur
-CLIENT_ID = "34e5e22dcc85836"
-image_link = pyimgur.Imgur(CLIENT_ID)
-
 # establishing connection to RabbitMQ server
 credentials = pika.PlainCredentials( username = 'guest' , password = 'guest' )
 connection = pika.BlockingConnection( pika.ConnectionParameters(
             host = 'message-broker' , port = 5672 , credentials = credentials ) )
 channel = connection.channel()
-
-# plotting Reflectivity range
-def plotting( plot_data ) :
-    # acessing dictionary data and making it into numpy
-    # arrays so that it can be used for doing mathemtical operations
-    # and plotting values
-    ref_range = np.array( plot_data[ "ref_range" ] )
-    rho_range = np.array( plot_data[ "rho_range" ] )
-    ref = np.array( plot_data[ "ref" ] )
-    rho = np.array( plot_data[ "rho" ] )
-    az = np.array( plot_data[ "az" ] )
-    fig, axes = plt.subplots( 1 , 2 , figsize = ( 15 , 8 ) )
-    for var_data, var_range, ax in zip( ( ref , rho ) , ( ref_range , rho_range ) , axes ) :
-        # Turn into an array, then mask
-        data = ma.array( var_data )
-        data[ np.isnan( data ) ] = ma.masked
-        # Convert az,range to x,y
-        xlocs = var_range * np.sin( np.deg2rad( az[ : , np.newaxis ] ) )
-        ylocs = var_range * np.cos( np.deg2rad( az[ : , np.newaxis ] ) )
-        # Plot the data
-        cmap = ctables.registry.get_colortable( 'viridis' )
-        ax.pcolormesh( xlocs , ylocs , data , cmap = cmap )
-        ax.set_aspect( 'equal' , 'datalim' )
-        ax.set_xlim( -40 , 20 )
-        ax.set_ylim( -30 , 30 )
-        add_timestamp( ax , datetime.now( ) , y = 0.02 , high_contrast = True )
-    # Labelling plot
-    fig.suptitle( 'Minimum and Maximum range of Reflectivity' )
-    # saving the file to be used in future
-    plt.savefig( "Reflectivity_Correlation.png" )
-    uploaded_image = image_link.upload_image( 'Reflectivity_Correlation.png' , title = "Image Hosted" )
-    return str( uploaded_image.link )
 
 def callback(ch, method, properties, body):
     user_data = json.loads( body )
